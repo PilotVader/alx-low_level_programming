ba2fca8eb25911d7047d49244c05018a15d1ee6d
PilotVader <pilotsamson@gmail.com>
2022-10-03 18:12:26 -0700

    0x18-dynamic_libraries

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,3 +1,3 @@
-#!/bin/bash                                                       
-gcc -Wall -Werror -Wextra -pedantic -fPIC -c *.c                  
-gcc -shared -Wl,-soname,liball.so -o liball.so *.o
+#!/bin/bash
+gcc *.c -c -fPIC
+gcc *.o -shared -o liball.so
